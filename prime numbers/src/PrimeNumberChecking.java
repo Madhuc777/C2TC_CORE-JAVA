d56e549ff4a8aeea15ee0a4ce13d75dabd5b3188
HP <HP@Akshith>
2022-06-06 12:16:49 +0530

    first commit

---
+++ prime numbers/src/PrimeNumberChecking.java
@@ -0,0 +1,20 @@
+
+public class Prime
+{     public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=30;
+		boolean prime=true;
+		{
+			for(int i=2;i<n;i++)
+			if(n%i==0)
+			{
+			System.out.println("false");
+			}
+			System.out.println("true");
+	
+		}
+	
+	
+	}
+
+}
