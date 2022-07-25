2328dce5eca9058f3b562940807583f72f242cd5
HP <HP@Akshith>
2022-07-25 23:03:22 +0530

    first commit

---
+++ Array implementation/src/com/OneDimensionalArray.java
@@ -0,0 +1,32 @@
+package com;
+
+public class OneDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]=new int [5];
+		int count=0;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=count;
+			count++;
+		System.out.println(arr[i]+" ");
+		}
+				
+}	
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	
+
+
